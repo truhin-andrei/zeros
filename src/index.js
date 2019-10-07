5ad447a6bb80f045c7299ac29699d6920c8e2f6f
truhin-andrei <truhin-andrei@bk.ru>
2019-10-07 22:35:34 +0700

    init: start the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let arr = [];
+  let number;
+  let countFive =0;
+
+  arr = expression.split('*');
+
+  let count = 0;
+
+  arr.forEach(item => {
+    //let countFive = 0;
+    count = 0;
+    for(let i = 0; i< item.length; i++){
+      if (item[i] === "!") count++;
+    }
+
+    let n = 1;
+    number = Number(item.replace(/!/g, ''));
+
+    while (number >= Math.pow(5, n)) {
+      if (count === 2) {
+        countFive += (Math.floor((number/Math.pow(5, n)))/2);
+        n++;
+        continue;
+      }
+      countFive += (Math.floor(number/Math.pow(5, n)));
+      n++;
+    }
+  });
+  return countFive;
 }
