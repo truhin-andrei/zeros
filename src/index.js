f8005248e5653fe397733b50bfd562f7587d055b
truhin-andrei <truhin-andrei@bk.ru>
2019-10-08 22:05:50 +0700

    feat: add final solution

--- src/index.js
+++ src/index.js
@@ -1,75 +1,70 @@
 module.exports = function zeros(expression) {
   let arr = [];
-    let number;
-    let countFive =0,
+  let countFive =0,
     countTwo =0,
-    countTen =0,
-    countZero =0,
     countFiveDouble=0;
   
-    arr = expression.split('*');
+  arr = expression.split('*');
   
-    let count = 0;
+  let count = 0;
   
-    arr.forEach(item => {
-      //let countFive = 0;
-      count = 0;
-      for(let i = 0; i< item.length; i++){
-        if (item[i] === "!") count++;
-      }
-  
-      let n = 1;
-      number = Number(item.replace(/!/g, ''));
+  arr.forEach(item => {
+      
+    count = 0;
 
-      if (count === 1) {
-        while (number >= Math.pow(5, n)) {
+    for(let i = 0; i< item.length; i++){
+      if (item[i] === "!") count++;
+    }
+  
+    let n = 1;
+    let number = Number(item.replace(/!/g, ''));
+
+    if (count === 1) {
+      while (number >= Math.pow(5, n)) {
         countFive += (Math.floor(number/Math.pow(5, n)));
         n++; 
-        }
+      }
+      n=1;
+      while (number >= Math.pow(2, n)) {
+        countTwo += (Math.floor(number/Math.pow(2, n)));
+        n++; 
+      }
     }
 
-      if ((count === 2) && (number%2 == 0)){
-        let correct = 0;
-        k=10;
-        while (number/(10*k)>=1){
-          correct +=Math.floor(number/(10*k));
-          k+=10;
-        }
-        while (number >= Math.pow(2, n)) {
-            countTwo += (Math.floor(number/Math.pow(2, n)));
-            if(number >= Math.pow(10, n)){
-            countFiveDouble += (Math.floor(number/Math.pow(10, n)));
-            }
-            n++; 
-        }
-        countFiveDouble += Math.floor(correct);
+    if ((count === 2) && (number%2 == 0)){
+      let correct = 0;
+        
+      if (number/(50)>=1){
+        correct ++;
       }
 
-      if ((count === 2) && (number%2 != 0)){
-        let correct = 0;
-        k=1;
-        while (number/(10*k)>1){
-          correct +=Math.floor(number/(10*k));
-          k*=5;
+      while (number >= Math.pow(2, n)) {
+        countTwo += (Math.floor(number/Math.pow(2, n)));
+        if(number >= Math.pow(10, n)){
+        countFiveDouble += (Math.floor(number/Math.pow(10, n)));
         }
+         n++; 
+      }
+      countFiveDouble += Math.floor(correct);
+    }
 
-        while (number >= Math.pow(5, n)) {
-          countFive += (Math.floor(number/Math.pow(5, n)));
-          n++; 
-        } 
-        countFive -= Math.floor(correct);
-           
+    if ((count === 2) && (number%2 != 0)){
+      let correct = 0;
+      k=1;
+      while (number/(10*k)>1){
+        correct +=Math.floor(number/(10*k));
+        k*=5;
+      }
+
+      while (number >= Math.pow(5, n)) {
+        countFive += (Math.floor(number/Math.pow(5, n)));
+        n++; 
       } 
-                  
-    });
+      countFive -= Math.floor(correct);
+    } 
+  });
 
-   /* if (countFive>(countTwo-countTen)){
-     
-    }
-
-    return Math.floor (countZero+countTwo+countTen);
-    if (countFive<(countTwo-countTen)){
-        return Math.floor (countZero+countFive+countTen)
-    }*/
-    return Math.floor (countFive+countFiveDouble/1);
-      }
+  if ((countFive+countFiveDouble)==0 || countTwo == 0) return 0;
+   
+  return Math.floor (countFive+countFiveDouble);
+}
