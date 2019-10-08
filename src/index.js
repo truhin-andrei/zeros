4f18d5907d9bc81fd6afc7e760ab74f87f7357db
truhin-andrei <truhin-andrei@bk.ru>
2019-10-08 20:19:28 +0700

    feat: solution

--- src/index.js
+++ src/index.js
@@ -21,35 +21,55 @@ module.exports = function zeros(expression) {
       let n = 1;
       number = Number(item.replace(/!/g, ''));
 
-      if ((count === 2) && (number%2 == 0)){
-        while (number >= Math.pow(2, n)) {
-            countTwo += (Math.floor(number/Math.pow(2, n)));
-            if(number >= Math.pow(10, n)){
-            countTen += (Math.floor(number/Math.pow(10, n)));
-        }
-            n++; 
-            }
-      }
-
-      if ((count === 2) && (number%2 != 0)){
+      if (count === 1) {
         while (number >= Math.pow(5, n)) {
-            countFive += (Math.floor(number/Math.pow(5, n)));
-            n++; 
-            } 
-    }
-
-    if (count === 1) {
-        while (number >= Math.pow(5, n)) {
-        countZero += (Math.floor(number/Math.pow(5, n)));
+        countFive += (Math.floor(number/Math.pow(5, n)));
         n++; 
         }
     }
-              
+
+      if ((count === 2) && (number%2 == 0)){
+        let correct = 0;
+        k=10;
+        while (number/(10*k)>=1){
+          correct +=Math.floor(number/(10*k));
+          k+=10;
+        }
+        while (number >= Math.pow(2, n)) {
+            countTwo += (Math.floor(number/Math.pow(2, n)));
+            if(number >= Math.pow(10, n)){
+            countFiveDouble += (Math.floor(number/Math.pow(10, n)));
+            }
+            n++; 
+        }
+        countFiveDouble += Math.floor(correct);
+      }
+
+      if ((count === 2) && (number%2 != 0)){
+        let correct = 0;
+        k=1;
+        while (number/(10*k)>1){
+          correct +=Math.floor(number/(10*k));
+          k*=5;
+        }
+
+        while (number >= Math.pow(5, n)) {
+          countFive += (Math.floor(number/Math.pow(5, n)));
+          n++; 
+        } 
+        countFive -= Math.floor(correct);
+           
+      } 
+                  
     });
-    if (countFive>(countTwo-countTen)){
-        return Math.floor (countZero+countTwo+countTen);
+
+   /* if (countFive>(countTwo-countTen)){
+     
     }
+
+    return Math.floor (countZero+countTwo+countTen);
     if (countFive<(countTwo-countTen)){
         return Math.floor (countZero+countFive+countTen)
-    }
+    }*/
+    return Math.floor (countFive+countFiveDouble/1);
       }
